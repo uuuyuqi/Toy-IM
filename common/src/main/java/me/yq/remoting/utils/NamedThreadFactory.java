12e240b54896104e745a42ca3e2916bcb9debe66
wuyuqi <vvuyuqi@gmail.com>
2023-04-03 00:06:41 +0800

    新增 线程 factory 增加 线程组 和 异常处理器 的功能

--- common/src/main/java/me/yq/remoting/utils/NamedThreadFactory.java
+++ common/src/main/java/me/yq/remoting/utils/NamedThreadFactory.java
@@ -1,5 +1,6 @@
 package me.yq.remoting.utils;
 
+import java.lang.Thread.UncaughtExceptionHandler;
 import java.util.Objects;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -11,29 +12,51 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class NamedThreadFactory implements ThreadFactory {
 
-    private final AtomicInteger index = new AtomicInteger(0);
-
     private final String namePrefix;
+
+    private final AtomicInteger index = new AtomicInteger(0);
 
     /**
      * 默认都是 daemon 线程 (daemon is true)
      */
     private final boolean daemon;
 
+    private final UncaughtExceptionHandler exceptionHandler;
+
+    private final ThreadGroup threadGroup;
+
+
     public NamedThreadFactory(String namePrefix) {
         this(namePrefix,true);
     }
 
     public NamedThreadFactory(String namePrefix, boolean daemon) {
+        this(namePrefix,daemon,null);
+    }
+
+    public NamedThreadFactory(String namePrefix, boolean daemon, UncaughtExceptionHandler exceptionHandler) {
+        this(namePrefix,daemon,exceptionHandler,null);
+    }
+
+    public NamedThreadFactory(String namePrefix, boolean daemon, UncaughtExceptionHandler exceptionHandler, ThreadGroup group) {
         this.namePrefix = Objects.requireNonNull(namePrefix);
         this.daemon = daemon;
+        this.exceptionHandler = exceptionHandler;
+        this.threadGroup = group;
     }
 
     @Override
     public Thread newThread(Runnable r) {
+        ThreadGroup group = this.threadGroup == null ? Thread.currentThread().getThreadGroup() : this.threadGroup;
         String threadName = "IM-" + namePrefix + "-" + index.getAndIncrement();
-        Thread newThread = new Thread(r,threadName);
+        Thread newThread = new Thread(group, r, threadName);
+
+        // 不管所处的 threadGroup 怎么设置，这里还是按照 daemon 参数来设置
         newThread.setDaemon(daemon);
+
+        if (this.exceptionHandler != null)
+            newThread.setUncaughtExceptionHandler(this.exceptionHandler);
+
         return newThread;
     }
 }
