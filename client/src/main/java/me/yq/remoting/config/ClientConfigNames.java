0b341b486cefc52a58575aae7d68a3a485516d05
wuyuqi <vvuyuqi@gmail.com>
2023-04-03 11:16:25 +0800

    新增注释

--- client/src/main/java/me/yq/remoting/config/ClientConfigNames.java
+++ client/src/main/java/me/yq/remoting/config/ClientConfigNames.java
@@ -6,30 +6,35 @@ package me.yq.remoting.config;
  */
 public class ClientConfigNames {
 
-
+    // 服务端地址
     public static final String REMOTE_SERVER_HOST = "REMOTE_SERVER_HOST";
-
+    // 服务端端口
     public static final String REMOTE_SERVER_PORT = "REMOTE_SERVER_PORT";
 
 
 
-
+    // 默认序列化方式
     public static final String DEFAULT_SERIALIZATION = "DEFAULT_SERIALIZATION"; // means hessian2
-
+    // 默认协议版本
     public static final String DEFAULT_PROTOCOL_VERSION = "DEFAULT_PROTOCOL_VERSION";
-
-    public static final String HEARTBEAT_IDLE_SECONDS = "HEARTBEAT_INTERVAL_SECONDS";
-
+    // 默认心跳间隔
+    public static final String HEARTBEAT_IDLE_SECONDS = "HEARTBEAT_IDLE_SECONDS";
+    // 默认心跳最大失败次数
     public static final String HEARTBEAT_MAX_FAIL_COUNT = "HEARTBEAT_MAX_FAIL_COUNT";
 
-    public static final String SHUTDOWN_TIMEOUT_MILLIS = "SHUTDOWN_TIMEOUT_MILLIS";
 
-
-
+    // 业务线程池核心数
     public static final String BIZ_CORE_THREAD_NUM = "BIZ_CORE_THREAD_NUM";
+    // 业务线程池最大数
     public static final String BIZ_MAX_THREAD_NUM = "BIZ_MAX_THREAD_NUM";
+    // 业务线程池存活时间
     public static final String BIZ_EXTRA_T_ALIVE_SECONDS = "BIZ_EXTRA_T_ALIVE_SECONDS";
-
-
+    // 心跳开启
     public static final String HEARTBEAT_ENABLE = "HEARTBEAT_ENABLE";
+
+
+    // 停机超时时间
+    public static final String SHUTDOWN_TIMEOUT_MILLIS = "SHUTDOWN_TIMEOUT_MILLIS";
+    // 等待响应消息超时时间
+    public static final String WAIT_RESPONSE_MILLIS = "WAIT_RESPONSE_MILLIS";
 }
--- server/src/main/java/me/yq/remoting/config/ServerConfigNames.java
+++ server/src/main/java/me/yq/remoting/config/ServerConfigNames.java
@@ -5,14 +5,31 @@ package me.yq.remoting.config;
  * @version v1.0 2023-03-31 13:48
  */
 public class ServerConfigNames {
+    // 服务端端口
     public static final String SERVER_PORT = "SERVER_PORT";
+
+    // 客户端请求建联 超时时间
     public static final String CONNECT_TIMEOUT_MILLIS = "CONNECT_TIMEOUT_MILLIS";
+    // 客户端无响应 判定死亡的时间
     public static final String CLIENT_TIMEOUT_SECONDS = "CLIENT_TIMEOUT_SECONDS";
+
+    // 停机超时时间
     public static final String SHUTDOWN_TIMEOUT_MILLIS = "SHUTDOWN_TIMEOUT_MILLIS";
+
+    // 业务线程池核心数
     public static final String BIZ_CORE_THREAD_NUM = "BIZ_CORE_THREAD_NUM";
+    // 业务线程池最大数
     public static final String BIZ_MAX_THREAD_NUM = "BIZ_MAX_THREAD_NUM";
+    // 业务线程池存活时间
     public static final String BIZ_EXTRA_T_ALIVE_SECONDS = "BIZ_EXTRA_T_ALIVE_SECONDS";
+
+    // 优雅移除单个 channel 超时时间
     public static final String REMOVE_TIMEOUT_MILLIS = "REMOVE_TIMEOUT_MILLIS";
+
+    // 心跳开启
     public static final String HEARTBEAT_ENABLE = "HEARTBEAT_ENABLE";
+
+    // 等待响应消息超时时间
+    public static final String WAIT_RESPONSE_MILLIS = "WAIT_RESPONSE_MILLIS";
 
 }
