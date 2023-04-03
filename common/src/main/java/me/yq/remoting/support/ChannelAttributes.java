117ee69820a72ce58b74579304a9db55aabf7997
wuyuqi <vvuyuqi@gmail.com>
2023-04-03 10:50:00 +0800

    修改 重命名常量名称

--- common/src/main/java/me/yq/remoting/support/ChannelAttributes.java
+++ common/src/main/java/me/yq/remoting/support/ChannelAttributes.java
@@ -10,7 +10,7 @@ public class ChannelAttributes {
     /**
      * 用来缓存请求任务，实现任务的乱序收发处理
      */
-    public static AttributeKey<RequestFutureMap> REQUEST_FUTURE_MAP = AttributeKey.valueOf("REQUEST_FUTURE_MAP");
+    public static AttributeKey<RequestFutureMap> CHANNEL_REQUEST_FUTURE_MAP = AttributeKey.valueOf("CHANNEL_REQUEST_FUTURE_MAP");
 
     /**
      * 用来表示 是否能接收来自 当前 channel 的请求。在优雅停机状态下，会设置为 false，但是可以给其发送响应。
