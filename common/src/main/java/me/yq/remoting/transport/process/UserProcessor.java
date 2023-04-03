01bbad252ee5f95d652da0e17c8f002a07a8fc9e
wuyuqi <vvuyuqi@gmail.com>
2023-04-03 20:57:18 +0800

    修改 将一些合理的指责从 UserProcessor 这个聚合对象中，移动到单个 processor 对象中

--- common/src/main/java/me/yq/remoting/transport/process/UserProcessor.java
+++ common/src/main/java/me/yq/remoting/transport/process/UserProcessor.java
@@ -12,6 +12,7 @@ import me.yq.remoting.command.DefaultResponseCommand;
 import me.yq.remoting.command.RemotingCommand;
 import me.yq.remoting.support.ChannelAttributes;
 import me.yq.remoting.support.RequestFutureMap;
+import me.yq.remoting.transport.CommandSendingDelegate;
 
 import java.util.Map;
 import java.util.Objects;
@@ -20,7 +21,8 @@ import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * requestProcessor 集合，负责接收业务层数据，选择合适的业务处理器，并进行业务的处理
+ * requestProcessor 聚合对象，负责接收业务层数据，选择合适的业务处理器，并进行业务的处理
+ *
  * @author yq
  * @version v1.0 2023-03-28 20:29
  */
@@ -30,7 +32,7 @@ public class UserProcessor {
 
     private ThreadPoolExecutor bizThreadPool;
 
-    private final Map<BizCode,RequestProcessor> bizProcessors = new ConcurrentHashMap<>(8);
+    private final Map<BizCode, RequestProcessor> bizProcessors = new ConcurrentHashMap<>(8);
 
 
     /**
@@ -44,49 +46,48 @@ public class UserProcessor {
     }
 
 
-    public void registerBizProcessors(BizCode code, RequestProcessor processor){
+    public void registerBizProcessors(BizCode code, RequestProcessor processor) {
         RequestProcessor processorOld = this.bizProcessors.put(code, Objects.requireNonNull(processor));
         if (processorOld != null)
             throw new UnsupportedOperationException("不允许重复注册 processors");
     }
 
-    public void processCommand(ChannelHandlerContext ctx, RemotingCommand command){
+    public void processCommand(ChannelHandlerContext ctx, RemotingCommand command) {
         if (command instanceof DefaultRequestCommand) {
             DefaultRequestCommand requestCommand = (DefaultRequestCommand) command;
-            processRequest(ctx,requestCommand);
-        }
-        else if (command instanceof DefaultResponseCommand) {
+            processRequest(ctx, requestCommand);
+        } else if (command instanceof DefaultResponseCommand) {
             DefaultResponseCommand responseCommand = (DefaultResponseCommand) command;
-            processResponse(ctx,responseCommand);
+            processResponse(ctx, responseCommand);
         }
     }
 
 
     /**
      * 处理业务请求
-     * @param ctx 当前 netty handler context，该参数可以用来做消息的回传
+     *
+     * @param ctx            当前 netty handler context，该参数可以用来做消息的回传
      * @param requestCommand 待处理的请求
      */
-    private void processRequest(ChannelHandlerContext ctx, DefaultRequestCommand requestCommand){
+    private void processRequest(ChannelHandlerContext ctx, DefaultRequestCommand requestCommand) {
         // process in IO thread
         // 当系统处理停机状态时，不会再处理客户端发来的新请求
         ChannelAttributes.ChannelState channelState = ctx.channel().attr(ChannelAttributes.CHANNEL_STATE).get();
-        if (channelState == ChannelAttributes.ChannelState.CANNOT_REQUEST){
+        if (channelState == ChannelAttributes.ChannelState.CANNOT_REQUEST) {
             BaseResponse response = new BaseResponse(ResponseStatus.SERVER_ERROR, "系统正在停机，无法接收请求！", null);
-            sendResponseIfNeed(requestCommand.getMessageId(),response,ctx);
-        }
-        else if (channelState == ChannelAttributes.ChannelState.CLOSED){
+            CommandSendingDelegate.sendResponseOneway(ctx, requestCommand.getMessageId(), response);
+        } else if (channelState == ChannelAttributes.ChannelState.CLOSED) {
             // todo 后面这个地方应该是 do noting，打这个日志是方便测试
             log.warn("检测到 closed 状态的 channel 仍然发来信息! 请检查 channel 状态变更为 closed 后的代码逻辑");
         }
 
         // process in BIZ thread
         else if (channelState == null /* request for the first time */
-                || channelState == ChannelAttributes.ChannelState.CAN_REQUEST){
+                || channelState == ChannelAttributes.ChannelState.CAN_REQUEST) {
             this.bizThreadPool.execute(() -> {
                 // 每次处理请求时，请求计数+1
                 this.currentRequestCounts.incrementAndGet();
-                doProcessRequest(ctx,requestCommand);
+                doProcessRequest(ctx, requestCommand);
                 this.currentRequestCounts.decrementAndGet();
             });
         }
@@ -95,68 +96,32 @@ public class UserProcessor {
     /**
      * 反序列化请求，并找到对应的业务处理器，开始真正地处理业务请求
      */
-    private void doProcessRequest(ChannelHandlerContext ctx,DefaultRequestCommand requestCommand){
+    private void doProcessRequest(ChannelHandlerContext ctx, DefaultRequestCommand requestCommand) {
 
-        // 1. deserialize
+        // 1. 反序列化
         requestCommand.deserialize();
         BaseRequest request = requestCommand.getAppRequest();
 
-        // 2. find processor
+        // 2. 查找对应的业务处理器
         RequestProcessor processor = this.bizProcessors.get(request.getBizCode());
         if (processor == null)
             throw new SystemException("未找到交易码[" + request.getBizCode() + "]的处理器！请检查交易码的合法性或者是否设计并装配了该类型交易的处理器");
-        // 设置 thread local 对象，在 processors 处理的过程中，可能会用到 channel 信息
-        processor.getChannelLocal().set(ctx.channel());
 
-        // 3. process
-        BaseResponse response;
-        try {
-            // 业务处理
-            response = processor.process(request);
-        } catch (Exception e) {
-            response = new BaseResponse(ResponseStatus.FAILED, e.getMessage(), e);
-        }
-
-        // 4.return
-        sendResponseIfNeed(requestCommand.getMessageId(),response,ctx);
-
+        // 3. 业务处理
+        processor.processRequest(ctx, requestCommand.getMessageId(), request);
     }
 
     /**
      * 处理业务响应
-     * @param ctx 当前 netty handler context，该参数可以用来获取当前 channel，进而获取到 channel 中的 Attr
+     *
+     * @param ctx             当前 netty handler context，该参数可以用来获取当前 channel，进而获取到 channel 中的 Attr
      * @param responseCommand 待处理的响应
      */
-    private void processResponse(ChannelHandlerContext ctx, DefaultResponseCommand responseCommand){
-        RequestFutureMap requestFutureMap = ctx.channel().attr(ChannelAttributes.REQUEST_FUTURE_MAP).get();
+    private void processResponse(ChannelHandlerContext ctx, DefaultResponseCommand responseCommand) {
+        RequestFutureMap requestFutureMap = ctx.channel().attr(ChannelAttributes.CHANNEL_REQUEST_FUTURE_MAP).get();
         // 将 response 传递给给请求处
         requestFutureMap.commitResponseCommand(responseCommand);
     }
-
-
-    /**
-     * 发送业务返回数据
-     * @param requestCmdId 响应的流水 id
-     * @param response 待返回的业务数据
-     * @param ctx 返回时使用的 channel context
-     */
-    private void sendResponseIfNeed(int requestCmdId, BaseResponse response, ChannelHandlerContext ctx){
-
-        if (response.getStatus() == ResponseStatus.NO_NEED_RESPONSE)
-            return;
-
-        DefaultResponseCommand responseCommand = new DefaultResponseCommand(requestCmdId);
-        responseCommand.setAppResponse(response);
-        responseCommand.serialize(); // 序列化
-        ctx.writeAndFlush(responseCommand).addListener(
-                future -> {
-                    if (!future.isSuccess())
-                        log.error("消息发送失败！原因：[{}]", future.cause().getMessage());
-                }
-        );
-    }
-
-
 
 
     public AtomicInteger getCurrentRequestCounts() {
