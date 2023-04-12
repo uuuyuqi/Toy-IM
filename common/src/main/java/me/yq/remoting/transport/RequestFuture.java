9a67502c31c2392269323776e6dd4198c3fe8a74
wuyuqi <vvuyuqi@gmail.com>
2023-04-13 00:20:51 +0800

    重构 将 RequestFuture 从 RequestFutureMap 中移除这一职责转交给 RequestFuture 自己
    主要是解决异步调用（返回 future）的情况下，RequestFuture 对象会逃逸出去，很难统一进行 remove。其实这样还是违背了封装特性，后面有空再想想办法看怎么继续优化。为了尽量避免内存泄露（因为每个 future 都持有了 map），所以 future 会以弱引用的形式将其依赖。

--- common/src/main/java/me/yq/remoting/transport/RequestFuture.java
+++ common/src/main/java/me/yq/remoting/transport/RequestFuture.java
@@ -3,6 +3,8 @@ package me.yq.remoting.transport;
 import me.yq.common.BaseResponse;
 import me.yq.remoting.command.DefaultResponseCommand;
 
+import java.lang.ref.WeakReference;
+
 /**
  * 调用任务 future 类，用于等待响应
  */
@@ -10,12 +12,19 @@ public abstract class RequestFuture {
 
     private final int messageId;
 
-    public RequestFuture(int messageId) {
+    /**
+     * 该 future 属于哪个 RequestFutureMap，可以直接通过 future 来进行 remove 操作，避免了额外的 map 操作
+     */
+    private final WeakReference<RequestFutureMap> belongsTo;
+
+    public RequestFuture(int messageId, RequestFutureMap belongsTo) {
         this.messageId = messageId;
+        this.belongsTo = new WeakReference<>(belongsTo);
     }
 
     /**
      * 获取响应，不同的子类实现会有不同获取响应的方法
+     *
      * @param timeoutMillis 获取响应的超时时间
      * @return DefaultResponseCommand 通信层响应对象
      */
@@ -30,6 +39,22 @@ public abstract class RequestFuture {
     public int getMessageId() {
         return messageId;
     }
+
+    /**
+     * 关闭 future，主要是将 future 自己从 RequestFutureMap 中移除。
+     * 因为到这个阶段，future 已经不会再被使用了，应该进行移除，否则会造成内存泄漏。
+     */
+    public void close() {
+        try {
+            RequestFutureMap requestFutureMap = belongsTo.get();
+            if (requestFutureMap != null) {
+                requestFutureMap.removeSuchFuture(messageId);
+            }
+        } catch (NullPointerException ignored) {
+            // npe? whatever!
+        }
+    }
+
 
     /**
      * 构造一个失败的响应，通常发生在内部响应失败的场景下，直接返回一个失败。<br/>
