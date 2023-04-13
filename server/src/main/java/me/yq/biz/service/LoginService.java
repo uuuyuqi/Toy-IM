8c50e8215b82fd30a63552c6286e8d0d003bc215
wuyuqi <vvuyuqi@gmail.com>
2023-04-13 21:44:38 +0800

    修改 将 RuntimeException 改为 BusinessException

--- server/src/main/java/me/yq/biz/service/LoginService.java
+++ server/src/main/java/me/yq/biz/service/LoginService.java
@@ -3,6 +3,7 @@ package me.yq.biz.service;
 import lombok.extern.slf4j.Slf4j;
 import me.yq.biz.domain.User;
 import me.yq.biz.repository.UserDao;
+import me.yq.common.exception.BusinessException;
 
 /**
  * 校验登录信息是否合法的服务
@@ -27,13 +28,14 @@ public enum LoginService {
         User userFound = userDao.findUser(user);
         if (userFound == null){
             log.error("用户名[{}]不存在！登录失败！",user.getUserId());
-            throw new RuntimeException("用户名不存在！");
+            throw new BusinessException("用户名不存在！");
         }
 
         if (!userFound.getPasswd().equals(user.getPasswd())){
             log.error("用户名或密码错误！登录失败！");
-            throw new RuntimeException("用户名或密码错误！");
+            throw new BusinessException("用户名或密码错误！");
         }
+
         // 登录成功后，将密码到的用户密码置空
         userFound.setPasswd(null);
         return userFound;
