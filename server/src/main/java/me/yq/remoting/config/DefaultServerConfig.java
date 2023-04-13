f7df8617409b5157cde7003931acba238fb0d308
wuyuqi <vvuyuqi@gmail.com>
2023-04-13 21:46:55 +0800

    修改 移动所在的包路径

--- server/src/main/java/me/yq/remoting/config/DefaultServerConfig.java
+++ server/src/main/java/me/yq/remoting/config/DefaultServerConfig.java
@@ -1,20 +1,22 @@
 package me.yq.remoting.config;
 
+import me.yq.remoting.support.Config;
+
 /**
  * @author yq
  * @version v1.0 2023-03-31 13:38
  */
-public class DefaultServerConfig extends Config{
+public class DefaultServerConfig extends Config {
     {
         putConfig(ServerConfigNames.SERVER_PORT,"9088");
         putConfig(ServerConfigNames.CONNECT_TIMEOUT_MILLIS,"3000");
-        putConfig(ServerConfigNames.CLIENT_TIMEOUT_SECONDS,"90000");
+        putConfig(ServerConfigNames.CLIENT_TIMEOUT_SECONDS,"90");
         putConfig(ServerConfigNames.SHUTDOWN_TIMEOUT_MILLIS,"5000");
         putConfig(ServerConfigNames.BIZ_CORE_THREAD_NUM,"20");
         putConfig(ServerConfigNames.BIZ_MAX_THREAD_NUM,"200");
         putConfig(ServerConfigNames.BIZ_EXTRA_T_ALIVE_SECONDS,"30");
         putConfig(ServerConfigNames.REMOVE_TIMEOUT_MILLIS,"3000");
-        putConfig(ServerConfigNames.HEARTBEAT_ENABLE,"true");
-
+        putConfig(ServerConfigNames.IDLE_CHECK_ENABLE,"true");
+        putConfig(ServerConfigNames.WAIT_RESPONSE_MILLIS,"3000");
     }
 }
